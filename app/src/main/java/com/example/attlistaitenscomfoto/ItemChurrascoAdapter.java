9988074e8c3bbd877acfd73281af18bcc77453df
Nicolas Ricardo Maba <nicolas.rm2005@aluno.ifsc.edu.br>
2023-09-13 12:17:41 -0300

    List

--- app/src/main/java/com/example/attlistaitenscomfoto/ItemChurrascoAdapter.java
+++ app/src/main/java/com/example/attlistaitenscomfoto/ItemChurrascoAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -31,6 +33,13 @@ public class ItemChurrascoAdapter extends ArrayAdapter<ItensChurrasco> {
 
         LayoutInflater inflater = LayoutInflater.from(mContext);
         convertView = inflater.inflate(mResource, parent, false);
+        TextView txtNome = convertView.findViewById(R.id.nome);
+        TextView txtId = convertView.findViewById(R.id.id);
+        ImageView txtImagem = convertView.findViewById(R.id.imagem);
+
+        txtNome.setText(getItem(position).nome);
+        txtId.setText(getItem(position).id);
+        txtImagem.setImageResource(getItem(position).imagem);
         return convertView;
 
         //return super.getView(position, convertView, parent);
